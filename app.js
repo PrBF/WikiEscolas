414e808271db67552d904e2722e6bf0e07a6280a
Inaê Rosa <inaedb.rosa@gmail.com>
2021-02-25 16:13:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require ("express");
 const wiki = express();
+const path = require('path');
+
+wiki.use(express.urlencoded({extended: true}));
+
+wiki.use(express.static(path.join(__dirname, 'public')));
+wiki.set ('view engine', 'ejs');
+wiki.set ('views', path.join(__dirname, 'views'));
 
 wiki.get("/", function(req,res){
     res.send ("Pagina inicial");
 })
 
-wiki.post("/cadastro_escolas", function(req,res){
-    res.send ("Cadastramento de escolas");
+wiki.get("/cadastro_escolas", function(req,res){
+    res.render ('cadastro');
 })
 
-wiki.post("/dados_escolas", function(req.res){
-    res.send ("Formulario recebido!");
+wiki.post("/dados_escolas", function(req,res){
+    const {nome_escola, endereco, INEP, responsavel} = req.body;
+    const { email, telefone, modalidade, tipoInst, horario, foto, ano} = req.body;
+
+    res.render ('dados_escolas', {nome_escola, endereco, INEP, responsavel, email, telefone, modalidade, tipoInst, horario, foto, ano});
 })
+
 
 wiki.get ("/visualiza_mapa", function(req,res){
     res.send ("Integração com API");
@@ -19,4 +30,4 @@ wiki.get ("/visualiza_mapa", function(req,res){
 
 
 
-wiki.listen(8050);
+wiki.listen(3000);
