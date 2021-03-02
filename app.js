ef0a1356cef32b0ac87eb27357e8c294e298d672
Inaê Rosa <inaedb.rosa@gmail.com>
2021-03-02 00:10:53 -0300

    rota_patch_escola

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 
 
 wiki.use(express.urlencoded({extended: true}));
-
+wiki.use(methodOverride('_method'));
 wiki.use(express.static(path.join(__dirname, 'public')));
 wiki.set ('view engine', 'ejs');
 wiki.set ('views', path.join(__dirname, 'views'));
@@ -27,6 +27,25 @@ wiki.post('/escolas', function(req,res){
 
 wiki.get('/escolas/:INEP/edit', (req,res) =>{
     res.send("atualização dos dados da escola");
+})
+
+wiki.patch('/escolas/:INEP', (req,res) =>{
+    const {codigo} = req.params;
+    const {nome_escola, endereco, responsavel} = req.body;
+    const novoNome = req.body.nome_escola;
+    const novoEndereco = req.body.endereco;
+    const novoResponsavel = req.body.responsavel;
+    const novoEmail = req.body.email;
+    const novoTelefone = req.body.telefone;
+    const atualizaModalidade = req.body.modalidade;
+    const atualizaTipoInst = req.body.tipoInst;
+    const atualizaHorario = req.body.horario;
+    const atualizaFoto = req.body.foto;
+    const atualizaAno = req.body.ano;
+    const { email, telefone, modalidade, tipoInst, horario, foto, ano} = req.body;
+    //const de filtro de busca
+    //atribuição
+    res.redirect('/escolas/'+ codigo);
 })
 
 wiki.delete('/escolas/:INEP', (req,res) =>{
