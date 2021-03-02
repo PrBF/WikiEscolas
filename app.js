ff03c060834b9c319e101199f41e16b666490654
Inaê Rosa <inaedb.rosa@gmail.com>
2021-03-01 23:29:14 -0300

    Atualização RESTful

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require ("express");
+const express = require ('express');
 const wiki = express();
 const path = require('path');
+const methodOverride = require('method-override');
+
 
 wiki.use(express.urlencoded({extended: true}));
 
@@ -8,26 +10,36 @@ wiki.use(express.static(path.join(__dirname, 'public')));
 wiki.set ('view engine', 'ejs');
 wiki.set ('views', path.join(__dirname, 'views'));
 
-wiki.get("/", function(req,res){
+wiki.get('/', function(req,res){
     res.send ("Pagina inicial");
 })
 
-wiki.get("/cadastro_escolas", function(req,res){
-    res.render ('cadastro');
+wiki.get('/escolas/cadastro_escolas', function(req,res){
+    res.render ('escolas/new');
 })
 
-wiki.post("/dados_escolas", function(req,res){
+wiki.post('/escolas', function(req,res){
     const {nome_escola, endereco, INEP, responsavel} = req.body;
     const { email, telefone, modalidade, tipoInst, horario, foto, ano} = req.body;
 
-    res.render ('dados_escolas', {nome_escola, endereco, INEP, responsavel, email, telefone, modalidade, tipoInst, horario, foto, ano});
+    res.render ('escolas/show', {nome_escola, endereco, INEP, responsavel, email, telefone, modalidade, tipoInst, horario, foto, ano});
 })
 
+wiki.get('/escolas/:INEP/edit', (req,res) =>{
+    res.send("atualização dos dados da escola");
+})
 
-wiki.get ("/visualiza_mapa", function(req,res){
+wiki.delete('/escolas/:INEP', (req,res) =>{
+    const {codigo} = req.params;
+    //filtro de código 
+    res.redirect('/escolas');
+})
+
+wiki.get ('/visualiza_mapa', function(req,res){
     res.send ("Integração com API");
 })
 
 
-
-wiki.listen(3000);
+wiki.listen(3000, () =>{
+    console.log("Servidor rodando!");
+});
