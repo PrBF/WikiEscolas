0ecf7b0405d078b713730f62e36bb41ba41f1e28
inaerosa <61328048+inaerosa@users.noreply.github.com>
2021-07-28 14:47:45 -0300

    conexão com o banco

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+mongoose.connect('mongodb://localhost:27017/dbWiki', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() =>{
+    console.log("Conexão com o banco estabelecida");
+})
+.catch(err =>{
+    console.log("Erro ao conectar com o banco....");
+    console.log(err);
+})
+
 app.get('/', (req, res) =>{
     res.render('index')
 })
