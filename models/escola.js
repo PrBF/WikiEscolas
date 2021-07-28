e2fc0ce6c93573d904b8a0b998bfd856cfde15ad
inaerosa <61328048+inaerosa@users.noreply.github.com>
2021-07-27 23:16:32 -0300

    definição de atributos

--- models/escola.js
+++ models/escola.js
@@ -1,7 +1,72 @@
 const mongoose = require('mongoose');
 
 const escolaSchema = new mongoose.Schema({
-
+    nome:{
+        type: String,
+        required: true
+    }, 
+    cnpj:{
+        type: String
+    },
+    endereco:{
+        type: String, 
+        required: true
+    }, 
+    id_inep: {
+        type: Number,
+        required: true
+    },
+    responsavel: {
+        type: String,
+        required: true
+    }, 
+    contato: {
+        email:{
+            type: String, 
+            required: true
+        },
+        telefone: {
+            tel1: {
+                type: Number,
+                required: true
+            }, 
+            tel2: {
+                type: Number, 
+                required: true
+            }
+        }, 
+        redes: {
+            facebook: {
+                type: String, 
+            }, 
+            youtube: {
+                type: String,
+            },
+            instagram: {
+                type: String
+            },
+            site: {
+                type: String
+            }, 
+            blog: {
+                type: String
+            }
+        }
+    }, 
+    username: {
+        type: String, 
+        required: true
+    },
+    foto: {
+        type: String,
+    }, 
+    horario_func: {
+        type: Date,
+        required: true
+    }, 
+    ano_fund : {
+        type: Date
+    }
 })
 
 
