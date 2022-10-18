6f8256ef66856c1672bfee3175504a39913f869d
Inaê Rosa <inaedb.rosa@gmail.com>
2022-10-18 11:02:32 -0300

    refactor: remove some configs and endpoints

--- app.js
+++ app.js
@@ -2,30 +2,25 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const methodOverride = require('method-override')
-const mongoose= require('mongoose');
 const Escola = require('./models/escola');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const flash = require('connect-flash')
+require('./config/db/connection');
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-mongoose.connect('mongodb://localhost:27017/dbWiki', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() =>{
-    console.log("Conexão com o banco estabelecida");
-})
-.catch(err =>{
-    console.log("Erro ao conectar com o banco....");
-    console.log(err);
-})
+app.use('/', require('./routes/index'));
+app.use('/api', require('./routes/api'))
 
 app.use(session({secret: 'my_secret...', resave: false, saveUninitialized: false}))
 app.use(passport.initialize());
@@ -48,36 +43,10 @@ const isLoggedIn = (req, res, next) =>{
     res.redirect("login");
 }
 
-app.get('/coordenadas', async (req, res) => {
-    const info = await Escola.find({});
-    const coordEscolas = [];
-    for (var i = 0; i < info.length; i++){
-        var objCor = {
-            id: info[i]._id,
-            nome: info[i].nome,
-            lat_log : [info[i].latitude, info[i].longitude]
-        }
-        coordEscolas.push(objCor);
-    }
-    res.json(coordEscolas);
-})
-
-app.get('/', (req, res) =>{
-    res.render('index')
-})
-
-
-app.get('/lgpd', (req, res) => {
-    res.render('lgpd')
-})
-
-app.get('/termos-e-condicoes', (req, res) => {
-    res.render('termos')
-})
-
-app.get('/show', (req, res) =>{
-    res.render('show')
-})
+app.get('/index', (req, res) => {
+    const resultados = undefined;
+    res.render('show', {resultados})
+});
 
 app.get('/login', (req, res) =>{
     const errors = req.flash().error || []
@@ -98,36 +67,24 @@ app.post('/login',
     })
 );
 
-app.get('/escola', isLoggedIn, async(req, res) => {
-    const id = req.user;
-    const escola = await Escola.findById(id);
-    res.render('escolas/index', {escola})
-})
-
-
-app.get('/escola/new', (req, res) =>{
-    res.render('escolas/new');
-})
-
 app.post ('/escola', async (req, res) =>{
+    const data = req.body;
+    const escola = new Escola (data)
 
-        const {nome, cnpj, endereco, latitude, longitude, id_inep, responsavel, email, tel1, tel2, facebook, instagram, site, blog, modalidade, tipo_inst, foto, horario_funcMin, horario_funcMax, ano_fund, calendario, proj_pol_pedag, username,  autorizacao} = req.body;
-        const escola = new Escola ({nome, cnpj, endereco,  latitude, longitude, id_inep, responsavel, email, tel1, tel2, facebook, instagram, site, blog, modalidade, tipo_inst, foto, horario_funcMin, horario_funcMax, username, ano_fund, calendario, proj_pol_pedag, autorizacao})
-
-        Escola.register(escola, req.body.password, (err) => {
-            if (err){
-                console.log(err)
-                res.render('escolas/new')
-            } else {
-                passport.authenticate("local")(req, res, () => {
-                    res.redirect('/escola')
-                })
-            }
-        })
-
+    Escola.register(escola, req.body.password, (err) => {
+      if (err){
+        const errors = req.flash().err || []
+        res.render('escolas/login', {errors});
+          res.render('escolas/new')
+      } else {
+          passport.authenticate("local")(req, res, () => {
+              res.redirect('/escola')
+          })
+      }
+    })
 })
 
-app.get('/escola/:id', async (req, res) =>{
+app.get('/:id', async (req, res) =>{
     const {id} = req.params;
     const escola = await Escola.findById(id);
     if (escola){
@@ -135,6 +92,12 @@ app.get('/escola/:id', async (req, res) =>{
     } else {
         res.render('error');
     }
+})
+
+app.get('/escola', isLoggedIn, async(req, res) => {
+    const id = req.user;
+    const escola = await Escola.findById(id);
+    res.render('escolas/index', {escola})
 })
 
 app.get('/escola/:id/edit', isLoggedIn, async(req, res) => {
@@ -307,6 +270,10 @@ app.delete('/escola/:id/projeto/:id_projeto', isLoggedIn, async (req, res) => {
     res.redirect('/escola')
 })
 
-app.listen(4000, () =>{
+app.get('/administrador',  (req, res) => {
+    res.render('administrador/index')
+});
+
+app.listen(3000, () =>{
     console.log("Rodando")
 })
