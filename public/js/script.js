20cafc1e7d56a84d6a65f9bbc68db068a7b37bb1
Inaê Rosa <inaedb.rosa@gmail.com>
2022-10-18 11:03:11 -0300

    add some things

--- public/js/script.js
+++ public/js/script.js
@@ -32,23 +32,27 @@ function closeModal(){
     modalContent.style.display="none"
 }
 
-var map = L.map('map', {
-    center: [-16.166073148734803, -51.550334113559636],
-    zoom: 5
-});
-    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=L2MMuN0QVt7kuMWSYezJ',{
-        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-fetch('http://localhost:4000/coordenadas')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++){
-            L.marker([data[i].lat_log[0], data[i].lat_log[1]]).addTo(map)
-            .bindPopup(`<a href="http://localhost:4000/escola/${data[i].id}">${data[i].nome}</a>`)
-        }
+function map(){
+    var map = L.map('map', {
+        center: [-16.166073148734803, -51.550334113559636],
+        zoom: 5
+    });
+        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=L2MMuN0QVt7kuMWSYezJ',{
+            attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        }).addTo(map);
     
-    })
+    fetch('http://localhost:3000/api')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (var i = 0; i < data.length; i++){
+                L.marker([data[i].lat_log[0], data[i].lat_log[1]]).addTo(map)
+                .bindPopup(`<a href="http://localhost:3000/${data[i].id}">${data[i].nome}</a>`)
+            }
+        
+        })
+}
+
+map();
 
