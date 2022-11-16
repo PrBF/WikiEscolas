de01a08cb97ae5293e6b3401d6b7a7348ef55d99
Inaê Rosa <inaedb.rosa@gmail.com>
2022-11-15 23:19:19 -0300

    chore: dependencies of leaflet search

--- public/js/script.js
+++ public/js/script.js
@@ -32,26 +32,49 @@ function closeModal(){
     modalContent.style.display="none"
 }
 
-function map(){
-    var map = L.map('map', {
-        center: [-16.166073148734803, -51.550334113559636],
-        zoom: 5
-    });
-        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=L2MMuN0QVt7kuMWSYezJ',{
-            attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        }).addTo(map);
-    
+async function map(){
+
     fetch('http://localhost:3000/api')
-        .then(function(response) {
-            return response.json();
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (var i = 0; i < data.length; i++){
+            L.marker([data[i].lat_log[0], data[i].lat_log[1]]).addTo(map)
+            .bindPopup(`<a href="http://localhost:3000/escola/${data[i].id}">${data[i].nome}</a>`)
+        }
+        var markersLayer = new L.LayerGroup();	
+	
+	    map.addLayer(markersLayer);
+
+	    var controlSearch = new L.Control.Search({
+	    	position:'topright',		
+	    	layer: markersLayer,
+	    	initial: false,
+	    	zoom: 20,
+	    });
+
+	    map.addControl( controlSearch, {container: 'findbox'} );
+    
+	    for(i in data) {
+	    	var nome = data[i].nome,	
+	    		lat_log = data[i].lat_log,		
+	    		marker = new L.Marker(new L.latLng(lat_log), {title: nome} );
+	    	marker.bindPopup(`<a href="http://localhost:3000/escola/${data[i].id}">${data[i].nome}</a>`)
+	    	markersLayer.addLayer(marker);
+	    }
+
+        $('#textsearch').on('keyup', function(e) {
+            controlSearch.searchText( e.target.value );    
         })
-        .then(function (data) {
-            for (var i = 0; i < data.length; i++){
-                L.marker([data[i].lat_log[0], data[i].lat_log[1]]).addTo(map)
-                .bindPopup(`<a href="http://localhost:3000/escola/${data[i].id}">${data[i].nome}</a>`)
-            }
-        
-        })
+    })
+    
+	var map = new L.Map('map', {center: [-16.166073148734803, -51.550334113559636],zoom: 5});	//set center from first location
+
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=L2MMuN0QVt7kuMWSYezJ',{attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'}).addTo(map);
+	map.addLayer(new L.TileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=L2MMuN0QVt7kuMWSYezJ'));	//base layer
+
+
 }
 
 map();
